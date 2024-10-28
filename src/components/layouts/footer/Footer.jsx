fa441c32a101330b2809c9956e8226f06206cda8
Tamara Enrique <tamarae868@gmail.com>
2024-10-28 12:05:24 -0300

    configuracion  y agregado de detalles en css

--- src/components/layouts/footer/Footer.jsx
+++ src/components/layouts/footer/Footer.jsx
@@ -1,5 +1,54 @@
+import "./footer.css";
+
 const Footer = () => {
-  return <h2>AQUI FOOTER</h2>;
+  return (
+    <div className="col-md-2 col-lg-3 col-xl-3 mx-auto mt-3">
+      <div className=" container text-center text-md-start">
+        <div className="row text-center text-md-start">
+          <div className="col-md-2 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h5 className="titulo text-uppercase mb-4 font-weight-bold text-primary">
+              ENVIOS
+            </h5>
+            <p>
+              <li className="fas fa-truck me-3"></li>CORREO ARGENTINO
+            </p>
+            <p>
+              <li className="fas fa-truck me-3"></li>ANDREANI
+            </p>
+          </div>
+          <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h5 className="titulo text-uppercase mb-4 font-weight-bold text-primary">
+              PREGUNTAS FRECUENTES
+            </h5>
+            <p>Trabajamos con plan reserva? SI</p>
+            <p>Hacemos cambios/devoluciones? SI</p>
+          </div>
+          <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h5 className=" titulo text-uppercase mb-4 font-weight-bold text-primary">
+              CONTACTANOS
+            </h5>
+
+            <p>
+              <li className="fas fa-home me-3"></li>San Juan 1212 CABA
+            </p>
+            <p>
+              <li className="fas fa-envelope me-3"></li>IbiShoes@gmail.com
+            </p>
+            <p>
+              <li className="fas fa-phone me-3"></li>421-2323
+            </p>
+          </div>
+
+          <div className="redes">
+            <h5 className="titulo">SEGUINOS EN NUESTRAS REDES</h5>
+            <li className="fab fa-facebook">FACEBOOK</li>
+            <li className="fab fa-twitter">TWITTER</li>
+            <li className="fab fa-whatsapp">WHATSAPP</li>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
